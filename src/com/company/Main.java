1591a6658a4bed04fb0b6c70b5dd1e1f55e8f0ec
mc-greq <greq19@gmail.com>
2018-12-07 14:20:57 +0100

    Added PreparedStatement to safeguard against SQL injection attacks

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,33 +2,75 @@ package com.company;
 
 import com.company.model.Artist;
 import com.company.model.Datasource;
+import com.company.model.SongArtist;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Datasource datasource = new Datasource();
-        if(!datasource.open()){
-            System.out.println("Cant open datasource");
+        if(!datasource.open()) {
+            System.out.println("Can't open datasource");
             return;
         }
 
-        List<Artist> artists = datasource.queryArtists(Datasource.ORDER_BY_ASC);
-        if(artists == null){
+        List<Artist> artists = datasource.queryArtists(5);
+        if(artists == null) {
             System.out.println("No artists!");
             return;
         }
 
-        for(Artist artist: artists){
+        for(Artist artist : artists) {
             System.out.println("ID = " + artist.getId() + ", Name = " + artist.getName());
         }
 
-        List<String> albumsForArtists = datasource.queryAlbumsForArtist("Iron Maiden", Datasource.ORDER_BY_ASC);
-        for(String album: albumsForArtists){
+        List<String> albumsForArtist =
+                datasource.queryAlbumsForArtist("Carole King", Datasource.ORDER_BY_ASC);
+
+        for(String album : albumsForArtist) {
             System.out.println(album);
         }
 
+        List<SongArtist> songArtists = datasource.queryArtistsForSong("Go Your Own Way", Datasource.ORDER_BY_ASC);
+        if(songArtists == null) {
+            System.out.println("Couldn't find the artist for the song");
+            return;
+        }
+
+        for(SongArtist artist : songArtists) {
+            System.out.println("Artist name = " + artist.getArtistName() +
+                    " Album name = " + artist.getAlbumName() +
+                    " Track = " + artist.getTrack());
+        }
+
+        datasource.querySongsMetadata();
+
+        int count = datasource.getCount(Datasource.TABLE_SONGS);
+        System.out.println("Number of songs is: " + count);
+
+        datasource.createViewForSongArtists();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter song title: ");
+        String title = scanner.nextLine();
+
+
+        songArtists = datasource.querySongInfoView(title);
+        if(songArtists.isEmpty()) {
+            System.out.println("Couldn't find the artist for the song");
+            return;
+        }
+
+        for(SongArtist artist : songArtists) {
+            System.out.println("FROM VIEW - Artist name = " + artist.getArtistName() +
+                    " Album name = " + artist.getAlbumName() +
+                    " Track number = " + artist.getTrack());
+        }
+
         datasource.close();
+
+        //SELECT name, album, track FROM artists_list WHERE title = ? OR artist = ?;
     }
 }
